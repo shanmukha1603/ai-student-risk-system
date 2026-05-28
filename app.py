0e3e72cc5849fcc0976efd294827749cabb1f9d0
shanmukh1603 <shanmukha1603@gmail.com>
2026-05-28 17:54:29 +0530

    fixed render port

--- app.py
+++ app.py
@@ -563,4 +563,5 @@ def dashboard():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
